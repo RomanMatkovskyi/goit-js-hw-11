e98093ca5673a527fdd62660091f9a25cc7a1e90
RomanMatkovskyi <roman.matkovskyii@gmail.com>
2023-06-25 20:38:43 +0200

    FinishMainPart

---
+++ src/js/dataArray.js
@@ -0,0 +1,47 @@
+import { createHeadlines } from './markupRender'
+import axios from 'axios';
+import Notiflix from 'notiflix';
+export let imageInfoArray = [];
+
+
+let loadMoreBtn = document.getElementById('loadMore');
+
+
+export async function getInfoArray(searchData, page, imgArray) {
+    try {
+      const response = await axios.get('https://pixabay.com/api/?', {
+        params: {
+          key: '37799813-d6baa13d55c299777f9561755',
+          q: searchData,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: 'true',
+          page: page,
+          per_page: '40',
+        },
+      });
+      imageInfoArray = imgArray;
+      let resObject = response.data;
+      hitsCounter = resObject.totalHits;
+      imageInfoArray.push(...resObject.hits);
+      if (resObject.hits.length !== 0) {
+        if (imageInfoArray.length < hitsCounter) {
+          loadMoreBtn.classList.remove('visually-hidden');
+          createHeadlines(imageInfoArray);
+        } else {
+          Notiflix.Notify.failure(
+            'We are sorry, but youve reached the end of search results.'
+          );
+          loadMoreBtn.classList.add('visually-hidden');
+        }
+      } else {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  
---
+++ src/js/markupRender.js
@@ -0,0 +1,31 @@
+let galleryEl = document.querySelector('.gallery');
+
+
+export function createHeadlines(headlines) {
+    headlines.forEach(headline => {
+      createHeadline(headline);
+    });
+  }
+  
+  function createHeadline({
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  }) {
+    let template = `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" class="img-photo" loading="lazy" />
+  <div class="card-description">
+  <ul class="info">
+        <li class="info-item">Likes: ${likes}</li>
+        <li class="info-item">Views: ${views}</li>
+        <li class="info-item">Comments: ${comments}</li>
+        <li class="info-item">Downloads: ${downloads}</li>
+      </ul>
+      </div>
+  </div>`;
+    galleryEl.insertAdjacentHTML('beforeend', template);
+  }
